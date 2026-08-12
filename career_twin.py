261b6655faf7f8f2a58f056f07f080d436ad3f77
banditejaswini4477-gif <banditejaswini4477@gmail.com>
2026-08-12 19:22:28 +0530

    DAY 2: Add skill analysis

--- career_twin.py
+++ career_twin.py
@@ -15,3 +15,46 @@ print("DSA:",dsa)
 print("Python:",python)
 print("SQL:",sql)
 print("ML:",ml)
+
+
+
+print("\n===== SKILL ANALYSIS =====")
+
+if java>=7:
+    print("java: Strong")
+elif java>=4:
+    print("java: Average")
+else:
+    print("java: Weak")
+
+
+if dsa>=7:
+    print("DSA: Strong")
+elif dsa>=4:
+    print("DSA: Average")
+else:
+    print("DSA: Weak")
+
+
+if python>=7:
+    print("python: Strong")
+elif python>=4:
+    print("python: Average")
+else:
+    print("python: Weak")
+
+
+if sql>=7:
+    print("SQL: Strong")
+elif sql>=4:
+    print("SQL: Average")
+else:
+    print("SQL: Weak")
+
+
+if ml>=7:
+    print("Machine Learning: Strong")
+elif ml>=4:
+    print("Machine Learning: Average")
+else:
+    print("Machine Learning: Weak")
